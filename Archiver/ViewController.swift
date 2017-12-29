e9ff736280c2a834ab90933103475a7b21142a78
Julian Kahnert <mail@juliankahnert.de>
2017-12-29 17:14:42 +0100

    initial commit with basic  UI

--- Archiver/ViewController.swift
+++ Archiver/ViewController.swift
@@ -9,19 +9,40 @@
 import Cocoa
 
 class ViewController: NSViewController {
-
+    @IBOutlet weak var descriptionField: NSTextField!
+    @IBOutlet weak var tagField: NSTextField!
+    @IBOutlet weak var filenameField: NSTextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
         // Do any additional setup after loading the view.
     }
-
+    
     override var representedObject: Any? {
         didSet {
         // Update the view, if already loaded.
         }
     }
-
-
+//    @IBAction func sayButtonClicked(_ sender: Any) {
+//        var name = nameField.stringValue
+//        if name.isEmpty {
+//            name = "World"
+//        }
+//        let greeting = "Hello \(name)!"
+//        helloLabel.stringValue = greeting
+//    }
+    
+    @IBAction func nextButtonClicked(_ sender: Any) {
+        let greeting = "NEXT"
+        filenameField.stringValue = greeting
+    }
+    @IBAction func previousButtonClicked(_ sender: Any) {
+        let greeting = "PREVIOUS"
+        filenameField.stringValue = greeting
+    }
+    @IBAction func saveButtonClicked(_ sender: Any) {
+    }
+    
 }
 
