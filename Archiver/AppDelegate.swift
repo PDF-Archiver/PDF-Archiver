a232f5c2007dca0fd290bdcacd69e7a807ee2d8e
Julian Kahnert <mail@juliankahnert.de>
2017-12-29 16:20:18 +0100

    Initial Commit

---
+++ Archiver/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  Archiver
+//
+//  Created by Julian Kahnert on 29.12.17.
+//  Copyright © 2017 Julian Kahnert. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+    func applicationDidFinishLaunching(_ aNotification: Notification) {
+        // Insert code here to initialize your application
+    }
+
+    func applicationWillTerminate(_ aNotification: Notification) {
+        // Insert code here to tear down your application
+    }
+
+
+}
+
---
+++ Archiver/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  Archiver
+//
+//  Created by Julian Kahnert on 29.12.17.
+//  Copyright © 2017 Julian Kahnert. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
