e8ef33e5ce163088e2f60ababb8ca6fd4f772963
Julian Kahnert <mail@juliankahnert.de>
2018-02-06 20:50:23 +0100

    add a tabbing order, thanks to: https://stackoverflow.com/questions/39096382/trying-to-set-the-tab-order-in-xcode-for-osx-app

---
+++ PDFArchiver/MyWindow.swift
@@ -0,0 +1,15 @@
+//
+//  MyWindow.swift
+//  PDF Archiver
+//
+//  Created by Julian Kahnert on 06.02.18.
+//  Copyright © 2018 Julian Kahnert. All rights reserved.
+//
+
+import Cocoa
+
+class MyWindow: NSWindow {
+    override func recalculateKeyViewLoop() {
+        // Remove. nextKeyView and makeFirstResponder seemed broken with this
+    }
+}
