9a03c19f3dac58d0f144d0adbfd36106bd8538be
Julian Kahnert <mail@juliankahnert.de>
2018-02-09 22:50:03 +0100

    select the "Simple Theme" color theme

--- PDFArchiver/ViewController.swift
+++ PDFArchiver/ViewController.swift
@@ -9,6 +9,13 @@
 import Quartz
 
 class ViewController: NSViewController {
+    //https://color.adobe.com/de/Simple-Theme-color-theme-10451401/
+    let color1 = CGColor.init(red: 0.793, green: 0.255, blue: 0.310, alpha: 1)
+    let color2 = CGColor.init(red: 0.903, green: 0.412, blue: 0.404, alpha: 1)
+    let color3 = CGColor.init(red: 0.980, green: 0.980, blue: 0.980, alpha: 1)
+    let color4 = CGColor.init(red: 0.131, green: 0.172, blue: 0.231, alpha: 1)
+    let color5 = CGColor.init(red: 0.213, green: 0.242, blue: 0.286, alpha: 1)
+
     var dataModelInstance = DataModel()
 
     @IBOutlet weak var pdfview: PDFView!
