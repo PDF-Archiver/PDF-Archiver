70225c1497fe5c51c63d9781c6677216522f5873
Julian Kahnert <mail@juliankahnert.de>
2018-02-14 22:54:45 +0100

    run build script on master branch

--- other/update-dmg.sh
+++ other/update-dmg.sh
@@ -4,6 +4,9 @@ SCRIPTPATH="$( cd "$(dirname "$0")" ; pwd -P )"
 
 cd "$SCRIPTPATH/.."
 
+git stash
+git checkout master
+
 xcodebuild
 dmgbuild -s "$SCRIPTPATH/settings.py" "PDF Archiver" "PDFArchiver.dmg"
 
@@ -11,3 +14,6 @@ scp PDFArchiver.dmg freenas:/mnt/zpool/configs/NGINX-files/app/
 
 rm -rf "$SCRIPTPATH/../build"
 rm "$SCRIPTPATH/../PDFArchiver.dmg"
+
+git checkout develop
+git stash apply
