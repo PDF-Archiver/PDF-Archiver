a90ece3e8e5cb5ad3ce1bd96903f16c0d209787a
Julian Kahnert <mail@juliankahnert.de>
2018-06-23 17:26:22 +0200

    add OpenSSL in XCode

---
+++ PDFArchiver/Modules/shim.h
@@ -0,0 +1,26 @@
+//
+//  shim.h
+//  PDFArchiver
+//
+//  Created by Julian Kahnert on 23.06.18.
+//  Copyright © 2018 Julian Kahnert. All rights reserved.
+//
+
+#ifndef __OPENSSL_SHIM_H__
+#define __OPENSSL_SHIM_H__
+
+#include <openssl/conf.h>
+#include <openssl/evp.h>
+#include <openssl/err.h>
+#include <openssl/bio.h>
+#include <openssl/ssl.h>
+#include <openssl/md4.h>
+#include <openssl/md5.h>
+#include <openssl/sha.h>
+#include <openssl/hmac.h>
+#include <openssl/rand.h>
+#include <openssl/ripemd.h>
+#include <openssl/pkcs12.h>
+#include <openssl/x509v3.h>
+
+#endif
