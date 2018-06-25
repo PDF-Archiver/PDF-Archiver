4eb7a43169f154c9dfbdbbd2e98d49e7ae6ffb91
Julian Kahnert <mail@juliankahnert.de>
2018-06-25 18:45:11 +0200

    add SwiftyLocalReceiptValidator

--- PDFArchiver/Modules/shim.h
+++ PDFArchiver/Modules/shim.h
@@ -9,18 +9,25 @@
 #ifndef __OPENSSL_SHIM_H__
 #define __OPENSSL_SHIM_H__
 
-#include <openssl/conf.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
-#include <openssl/bio.h>
-#include <openssl/ssl.h>
-#include <openssl/md4.h>
-#include <openssl/md5.h>
-#include <openssl/sha.h>
-#include <openssl/hmac.h>
-#include <openssl/rand.h>
-#include <openssl/ripemd.h>
-#include <openssl/pkcs12.h>
-#include <openssl/x509v3.h>
+//#include <openssl/conf.h>
+//#include <openssl/evp.h>
+//#include <openssl/err.h>
+//#include <openssl/bio.h>
+//#include <openssl/ssl.h>
+//#include <openssl/md4.h>
+//#include <openssl/md5.h>
+//#include <openssl/sha.h>
+//#include <openssl/hmac.h>
+//#include <openssl/rand.h>
+//#include <openssl/ripemd.h>
+//#include <openssl/pkcs12.h>
+//#include <openssl/x509v3.h>
+//#include <openssl/pkcs7_union_accessors.h>
+
+#include "pkcs7_union_accessors.h"
+#import <openssl/pkcs7.h>
+#import <openssl/objects.h>
+#import <openssl/sha.h>
+#import <openssl/x509.h>
 
 #endif
---
+++ PDFArchiver/pkcs7_union_accessors.c
@@ -0,0 +1,49 @@
+//
+//  pkcs7_union_accessors.c
+//  PDFArchiver
+//
+//  Created by Julian Kahnert on 25.06.18.
+//  Copyright © 2018 Julian Kahnert. All rights reserved.
+//
+
+//
+//  pkcs7_union_accessors.c
+//  SwiftyReceiptValidatorDemo
+//
+//  Created by Andrew Bancroft on 10/26/17.
+//  Copyright © 2017 Andrew Bancroft. All rights reserved.
+//
+
+#include "pkcs7_union_accessors.h"
+
+inline char *pkcs7_d_char(PKCS7 *ptr) {
+    return ptr->d.ptr;
+}
+
+inline ASN1_OCTET_STRING *pkcs7_d_data(PKCS7 *ptr) {
+    return ptr->d.data;
+}
+
+inline PKCS7_SIGNED *pkcs7_d_sign(PKCS7 *ptr) {
+    return ptr->d.sign;
+}
+
+inline PKCS7_ENVELOPE *pkcs7_d_enveloped(PKCS7 *ptr) {
+    return ptr->d.enveloped;
+}
+
+inline PKCS7_SIGN_ENVELOPE *pkcs7_d_signed_and_enveloped(PKCS7 *ptr) {
+    return ptr->d.signed_and_enveloped;
+}
+
+inline PKCS7_DIGEST *pkcs7_d_digest(PKCS7 *ptr) {
+    return ptr->d.digest;
+}
+
+inline PKCS7_ENCRYPT *pkcs7_d_encrypted(PKCS7 *ptr) {
+    return ptr->d.encrypted;
+}
+
+inline ASN1_TYPE *pkcs7_d_other(PKCS7 *ptr) {
+    return ptr->d.other;
+}
