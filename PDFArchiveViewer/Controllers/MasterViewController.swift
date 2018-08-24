ec8856a35be86ff265905719c8714576700449ab
Julian Kahnert <mail@juliankahnert.de>
2018-08-24 22:33:59 +0200

    restructure the project

---
+++ PDFArchiveViewer/Controllers/MasterViewController.swift
@@ -0,0 +1,175 @@
+/**
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+
+class MasterViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+  
+  // MARK: - Properties
+  @IBOutlet var tableView: UITableView!
+  @IBOutlet var searchFooter: SearchFooter!
+  
+  var detailViewController: DetailViewController? = nil
+  var candies = [Candy]()
+  var filteredCandies = [Candy]()
+  let searchController = UISearchController(searchResultsController: nil)
+  
+  // MARK: - View Setup
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    // Setup the Search Controller
+    searchController.searchResultsUpdater = self
+    searchController.obscuresBackgroundDuringPresentation = false
+    searchController.searchBar.placeholder = "Search Candies"
+    navigationItem.searchController = searchController
+    definesPresentationContext = true
+    
+    // Setup the Scope Bar
+    searchController.searchBar.scopeButtonTitles = ["All", "Chocolate", "Hard", "Other"]
+    searchController.searchBar.delegate = self
+    
+    // Setup the search footer
+    tableView.tableFooterView = searchFooter
+    
+    candies = [
+      Candy(category:"Chocolate", name:"Chocolate Bar"),
+      Candy(category:"Chocolate", name:"Chocolate Chip"),
+      Candy(category:"Chocolate", name:"Dark Chocolate"),
+      Candy(category:"Hard", name:"Lollipop"),
+      Candy(category:"Hard", name:"Candy Cane"),
+      Candy(category:"Hard", name:"Jaw Breaker"),
+      Candy(category:"Other", name:"Caramel"),
+      Candy(category:"Other", name:"Sour Chew"),
+      Candy(category:"Other", name:"Gummi Bear"),
+      Candy(category:"Other", name:"Candy Floss"),
+      Candy(category:"Chocolate", name:"Chocolate Coin"),
+      Candy(category:"Chocolate", name:"Chocolate Egg"),
+      Candy(category:"Other", name:"Jelly Beans"),
+      Candy(category:"Other", name:"Liquorice"),
+      Candy(category:"Hard", name:"Toffee Apple")]
+    
+    if let splitViewController = splitViewController {
+      let controllers = splitViewController.viewControllers
+      detailViewController = (controllers[controllers.count-1] as! UINavigationController).topViewController as? DetailViewController
+    }
+  }
+  
+  override func viewWillAppear(_ animated: Bool) {
+    if splitViewController!.isCollapsed {
+      if let selectionIndexPath = tableView.indexPathForSelectedRow {
+        tableView.deselectRow(at: selectionIndexPath, animated: animated)
+      }
+    }
+    super.viewWillAppear(animated)
+  }
+  
+  override func didReceiveMemoryWarning() {
+    super.didReceiveMemoryWarning()
+  }
+  
+  // MARK: - Table View
+  func numberOfSections(in tableView: UITableView) -> Int {
+    return 1
+  }
+  
+  func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    if isFiltering() {
+      searchFooter.setIsFilteringToShow(filteredItemCount: filteredCandies.count, of: candies.count)
+      return filteredCandies.count
+    }
+    
+    searchFooter.setNotFiltering()
+    return candies.count
+  }
+  
+  func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+    let cell = tableView.dequeueReusableCell(withIdentifier: "Cell", for: indexPath)
+    let candy: Candy
+    if isFiltering() {
+      candy = filteredCandies[indexPath.row]
+    } else {
+      candy = candies[indexPath.row]
+    }
+    cell.textLabel!.text = candy.name
+    cell.detailTextLabel!.text = candy.category
+    return cell
+  }
+  
+  // MARK: - Segues
+  override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+    if segue.identifier == "showDetail" {
+      if let indexPath = tableView.indexPathForSelectedRow {
+        let candy: Candy
+        if isFiltering() {
+          candy = filteredCandies[indexPath.row]
+        } else {
+          candy = candies[indexPath.row]
+        }
+        let controller = (segue.destination as! UINavigationController).topViewController as! DetailViewController
+        controller.detailCandy = candy
+        controller.navigationItem.leftBarButtonItem = splitViewController?.displayModeButtonItem
+        controller.navigationItem.leftItemsSupplementBackButton = true
+      }
+    }
+  }
+  
+  // MARK: - Private instance methods
+  
+  func filterContentForSearchText(_ searchText: String, scope: String = "All") {
+    filteredCandies = candies.filter({( candy : Candy) -> Bool in
+      let doesCategoryMatch = (scope == "All") || (candy.category == scope)
+      
+      if searchBarIsEmpty() {
+        return doesCategoryMatch
+      } else {
+        return doesCategoryMatch && candy.name.lowercased().contains(searchText.lowercased())
+      }
+    })
+    tableView.reloadData()
+  }
+  
+  func searchBarIsEmpty() -> Bool {
+    return searchController.searchBar.text?.isEmpty ?? true
+  }
+  
+  func isFiltering() -> Bool {
+    let searchBarScopeIsFiltering = searchController.searchBar.selectedScopeButtonIndex != 0
+    return searchController.isActive && (!searchBarIsEmpty() || searchBarScopeIsFiltering)
+  }
+}
+
+extension MasterViewController: UISearchBarDelegate {
+  // MARK: - UISearchBar Delegate
+  func searchBar(_ searchBar: UISearchBar, selectedScopeButtonIndexDidChange selectedScope: Int) {
+    filterContentForSearchText(searchBar.text!, scope: searchBar.scopeButtonTitles![selectedScope])
+  }
+}
+
+extension MasterViewController: UISearchResultsUpdating {
+  // MARK: - UISearchResultsUpdating Delegate
+  func updateSearchResults(for searchController: UISearchController) {
+    let searchBar = searchController.searchBar
+    let scope = searchBar.scopeButtonTitles![searchBar.selectedScopeButtonIndex]
+    filterContentForSearchText(searchController.searchBar.text!, scope: scope)
+  }
+}
---
+++ PDFArchiveViewer/Views/SearchFooter.swift
@@ -0,0 +1,88 @@
+/**
+ * Copyright (c) 2017 Razeware LLC
+ *
+ * Permission is hereby granted, free of charge, to any person obtaining a copy
+ * of this software and associated documentation files (the "Software"), to deal
+ * in the Software without restriction, including without limitation the rights
+ * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the Software is
+ * furnished to do so, subject to the following conditions:
+ *
+ * The above copyright notice and this permission notice shall be included in
+ * all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+ * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+ * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+ * AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+ * LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
+ * THE SOFTWARE.
+ */
+
+import UIKit
+
+class SearchFooter: UIView {
+  
+  let label: UILabel = UILabel()
+  
+  override public init(frame: CGRect) {
+    super.init(frame: frame)
+    configureView()
+  }
+  
+  required public init?(coder: NSCoder) {
+    super.init(coder: coder)
+    configureView()
+  }
+  
+  func configureView() {
+    backgroundColor = UIColor.candyGreen
+    alpha = 0.0
+    
+    // Configure label
+    label.textAlignment = .center
+    label.textColor = UIColor.white
+    addSubview(label)
+  }
+  
+  override func draw(_ rect: CGRect) {
+    label.frame = bounds
+  }
+  
+  //MARK: - Animation
+  
+  fileprivate func hideFooter() {
+    UIView.animate(withDuration: 0.7) {[unowned self] in
+      self.alpha = 0.0
+    }
+  }
+  
+  fileprivate func showFooter() {
+    UIView.animate(withDuration: 0.7) {[unowned self] in
+      self.alpha = 1.0
+    }
+  }
+}
+
+extension SearchFooter {
+  //MARK: - Public API
+  
+  public func setNotFiltering() {
+    label.text = ""
+    hideFooter()
+  }
+  
+  public func setIsFilteringToShow(filteredItemCount: Int, of totalItemCount: Int) {
+    if (filteredItemCount == totalItemCount) {
+      setNotFiltering()
+    } else if (filteredItemCount == 0) {
+      label.text = "No items match your query"
+      showFooter()
+    } else {
+      label.text = "Filtering \(filteredItemCount) of \(totalItemCount)"
+      showFooter()
+    }
+  }
+
+}
