1dfef76cd29a1f3b8a6f8163ec4c1b7b71a1e2b9
Julian Kahnert <mail@juliankahnert.de>
2018-11-02 11:16:28 +0100

    install script update

--- scripts/install_app.sh
+++ scripts/install_app.sh
@@ -5,9 +5,14 @@ REPO_FOLDER="$(git rev-parse --show-toplevel)"
 # build OpenSSL
 bash "$REPO_FOLDER/scripts/build_openssl.sh"
 
+# workaroung added from: https://github.com/CocoaPods/CocoaPods/issues/8000
+export EXPANDED_CODE_SIGN_IDENTITY=""
+export EXPANDED_CODE_SIGN_IDENTITY_NAME=""
+export EXPANDED_PROVISIONING_PROFILE=""
+
 # build the app
 xcodebuild clean
-xcodebuild build -quiet -configuration Debug CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO
+xcodebuild build -quiet -workspace PDFArchiver.xcworkspace -scheme PDFArchiver -configuration Debug CODE_SIGN_IDENTITY="" CODE_SIGNING_REQUIRED=NO CODE_SIGNING_ALLOWED=NO CONFIGURATION_BUILD_DIR="$REPO_FOLDER/build/"
 
 # copy the app
-cp -r "$REPO_FOLDER/build/Debug/PDFArchiver.app" "$HOME/Applications/"
+cp -r "$REPO_FOLDER/build/PDFArchiver.app" "$HOME/Applications/"
