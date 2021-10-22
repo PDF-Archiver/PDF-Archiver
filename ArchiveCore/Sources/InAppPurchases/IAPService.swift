3363324fc962a628c57af7a5797b7a7de7cd2559
Julian Kahnert <mail@juliankahnert.de>
2021-10-22 17:18:31 +0200

    add version in plist

--- ArchiveCore/Sources/InAppPurchases/IAPService.swift
+++ ArchiveCore/Sources/InAppPurchases/IAPService.swift
@@ -42,7 +42,7 @@ public final class IAPService: NSObject, Log {
             appUsagePermitted = true
             return
         }
-        
+
         refreshReceiptIfNeeded()
         paymentQueue.add(self)
 
