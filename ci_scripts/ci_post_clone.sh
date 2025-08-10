b8eecb07b7fe87d795ace1c4ed632e389094e02b
Julian Kahnert <mail@juliankahnert.de>
2025-08-10 21:56:16 +0200

    fix build

--- ci_scripts/ci_post_clone.sh
+++ ci_scripts/ci_post_clone.sh
@@ -2,4 +2,41 @@
 
 # https://stackoverflow.com/a/78572430
 mkdir -p ~/Library/org.swift.swiftpm/security/
+
+# update the macros fingerprints according to the Package.resolved file
+PACKAGE_RESOLVED="../PDFArchiver.xcworkspace/xcshareddata/swiftpm/Package.resolved"
+
+# List of "packageIdentity targetName" pairs
+PACKAGES="
+swift-dependencies DependenciesMacrosPlugin
+swift-composable-architecture ComposableArchitectureMacros
+swift-case-paths CasePathsMacros
+swift-perception PerceptionMacros
+"
+
+entries=""
+
+# Loop through packages
+echo "$PACKAGES" | while read -r pkg target; do
+  [ -z "$pkg" ] && continue
+
+  # Extract fingerprint (revision) using jq
+  fingerprint=$(jq -r --arg pkg "$pkg" \
+    '.pins[] | select(.identity == $pkg) | .state.revision' \
+    "$PACKAGE_RESOLVED")
+
+  if [ -z "$fingerprint" ] || [ "$fingerprint" = "null" ]; then
+    echo "Error: Could not find fingerprint for package '$pkg'" >&2
+    exit 1
+  fi
+
+  # Create JSON entry
+  jq -n \
+    --arg fp "$fingerprint" \
+    --arg pkg "$pkg" \
+    --arg target "$target" \
+    '{fingerprint: $fp, packageIdentity: $pkg, targetName: $target}'
+done | jq -s '.' > macros.json
+
+# copy the new file
 cp macros.json ~/Library/org.swift.swiftpm/security/
