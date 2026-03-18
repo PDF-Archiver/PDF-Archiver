c307e6c39693d241930ad4b787a80546b3aa2cc4
Julian Kahnert <mail@juliankahnert.de>
2026-03-18 07:15:18 +0100

    refactor: simplify App Intents by removing dependency injection
    - Remove IntentNavigation protocol and IntentNavigationModel
    - Open deep link URLs directly in intent perform() methods
    - Remove AppDependencyManager registration from iOS and macOS entry points
    - Fixes "internal error" when running intents from Shortcuts

--- iOS/PDFArchiverIOSApp.swift
+++ iOS/PDFArchiverIOSApp.swift
@@ -5,17 +5,12 @@
 //  Created by Julian Kahnert on 16.06.24.
 //
 
-import AppIntents
 import ArchiverFeatures
 import Foundation
 import SwiftUI
 
 @main
 struct PDFArchiverIOSApp: App {
-    init() {
-        AppDependencyManager.shared.add(dependency: IntentNavigationModel.shared)
-    }
-
     var body: some Scene {
         WindowGroup {
             RootView()
--- macOS/PDFArchiverMacApp.swift
+++ macOS/PDFArchiverMacApp.swift
@@ -5,17 +5,12 @@
 //  Created by Julian Kahnert on 26.03.24.
 //
 
-import AppIntents
 import ArchiverFeatures
 import Foundation
 import SwiftUI
 
 @main
 struct PDFArchiverMacApp: App {
-    init() {
-        AppDependencyManager.shared.add(dependency: IntentNavigationModel.shared)
-    }
-
     var body: some Scene {
         WindowGroup {
             RootView()
